e0fdef66a0c2461efb20791ed79a800f9f27fc9c
mikeotis123 <michael.otis2006@gmail.com>
2017-12-08 19:13:53 -0600

    This is a simple js app that checks to see if you are old enough to enter the bar.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var age = prompt("How old are you?");
+
+
+if (age <= 0  ) {
+	alert("Error");
+}
+// else if (age >= 18) {
+	// alert("come on in but you cannot drink");
+// }
+else if (age >= 21 ){
+	alert("come on it and drink");
+}
+
+else {
+	alert("Sorry to young.");
+}
